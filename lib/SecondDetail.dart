311cee3ca5a355013a6f1fa96a54ad45784d58b5
주용준 <juyongjun@juyongjun-ui-iMac.local>
2022-06-06 23:12:30 +0900

    navigation(네비게이션)

---
+++ lib/SecondDetail.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SecondDetail extends StatelessWidget {
+  TextEditingController controller = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: [
+              TextField(
+                controller: controller,
+                keyboardType: TextInputType.text,
+              ),
+              ElevatedButton(
+                onPressed: (){
+                  // Navigator.of(context).pop();
+                  // Navigator.of(context).pushReplacementNamed('/third');
+                  Navigator.of(context).pop(controller .value.text);
+                },
+                // child: Text('Go to the first page'),
+                child: Text('SAVE'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
